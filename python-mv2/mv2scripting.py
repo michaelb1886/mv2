06bd7093ca2f72a93856856205518327296b7307
Michael Barson <michaelb1886@gmail.com>
2025-03-20 09:57:48 +1100

    added make register function

--- python-mv2/mv2scripting.py
+++ python-mv2/mv2scripting.py
@@ -18,6 +18,102 @@ def modifyScriptXML(scriptXMLFile, newScriptXMLFile, changes={}):
     with open(newScriptXMLFile, 'w') as f:
         f.writelines(lines)
 
+def makeRegister(settings):
+    
+    """
+    measurement range table
+    bit 0 | bit 1 | option
+    =======================
+    0     | 0     |  Bx
+    0     | 1     |  By
+    1     | 0     |  Bz
+    1     | 1     |  T
+    """
+    match settings.get("measurement_axis", "Bx"):
+        case "Bx":
+            register1 = 0b00
+        case "By":
+            register1 = 0b01
+        case "Bz":
+            register1 = 0b10
+        case "T":
+            register1 = 0b11
+        case _:
+            raise ValueError("Invalid measurement range")
+    
+    
+    """
+    sensings range table
+    bit 0 | bit 1 | range | option
+    ===============================
+    0     | 0     | 0.1 T |   0
+    0     | 1     | 0.3 T |   1
+    1     | 0     | 1 T   |   2
+    1     | 1     | 3 T   |   3
+    """
+
+    
+    match settings.get("sensing_range", 0):
+        case 0:
+            register2 = 0b00
+        case 1:
+            register2 = 0b01
+        case 2:
+            register2 = 0b10
+        case 3:
+            register2 = 0b11
+        case _:
+            raise ValueError("Invalid sensing range")
+    
+    
+    """
+    resolution table
+    bit 0 | bit 1 | num bit | refresh rate | option"
+    ================================================
+    0     | 0     | 14      |  3 kHz       | 0
+    0     | 1     | 15      |  1.5 kHz     | 1
+    1     | 0     | 16      |  0.75 kHz    | 2
+    1     | 1     | 16      |  0.375 kHz   | 3
+    """
+
+    match settings.get("resolution", 0):
+        case 0:
+            register3 = 0b00
+        case 1:
+            register3 = 0b01
+        case 2:
+            register3 = 0b10
+        case 3:
+            register3 = 0b11
+        case _:
+            raise ValueError("Invalid resolution")
+        
+    """
+    measurment axis table
+    bit 0 | bit 1 | axis | option
+    ===============================
+    0     | 0     | 3    | 0
+    0     | 1     | x    | 1
+    1     | 0     | y    | 2
+    1     | 1     | z    | 3
+    """
+    
+    match settings.get("output", 3):
+        case 3:
+            register4 = 0b00
+        case "x":
+            register4 = 0b01
+        case "y":
+            register4 = 0b10
+        case "z":
+            register4 = 0b11
+        case _:
+            raise ValueError("Invalid measurement axis")
+    
+    return register1<<6 | register2<<4 | register3<<2 | register4
+    
+
+
 def modifyXMLsnippet(type,value,lines):
     for i,line in enumerate(lines):
         m = re.search(f"<type>{type}</type>",line)
@@ -41,6 +137,4 @@ def getNewMXRPath(name=None,mxrFolderPath=mxrFolderPath):
         return mxrFolderPath + "newMXR_" + str(i) + ".mxr"
 
 if __name__ == "__main__":
-    scriptfile = "Tag_18_06_27_C/MV2/host/script/MV2DigitalScript.xml"
-    newScriptfile = "Tag_18_06_27_C/MV2/host/script/MV2DigitalScript_new.xml"
-    modifyScriptXML(scriptfile,newScriptfile,changes={"2C":"fish"})
+    makeRegister({"measurement_range":"Bx","sensing_range":0,"resolution":0,"measurement_axis":3})
